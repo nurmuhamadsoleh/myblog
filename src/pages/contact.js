807a8434314f6f6c6034f244e47fda4d98d23b96
NUr Muhamad Soleh <nursoleh33@gmail.com>
2021-01-01 15:24:09 +0700

    update contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,7 +8,7 @@ const ContactPage = () => {
     <Layout>
       <SEO title="Madu Abu Bakar Asli" />
       <h1>Contact</h1>
-      <p>Hubungi Admin Madu Abu Bakar di <a href="https://wa.me/+6285312261634" target="_blank">maduabubakar</a> Wa Chat.</p>
+      <p>Hubungi Muhamad Soleh Distibutor Madu Abu Bakar di <a href="https://wa.me/+6289608046965" target="_blank">maduabubakar</a> Wa Chat.</p>
     </Layout>
   )
 }
